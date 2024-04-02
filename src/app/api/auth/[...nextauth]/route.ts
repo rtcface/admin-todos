af73d178d6f9beffd7548e5e1a1065f9e76a84e6
Ramiro Tepehua Cortes <rtcface@gmail.com>
2024-04-01 22:38:29 -0600

    se agrega schema para auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -17,6 +17,29 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async jwt({ token, user, account, profile }) {
+      const dbUser = await prisma.user.findUnique({
+        where: { email: token.email ?? "no-email" },
+      });
+      token.roles = dbUser?.roles ?? ["no-roles"];
+      token.id = dbUser?.id ?? "no-id";
+      return token;
+    },
+    async session({ session, token, user }) {
+      if (session && session.user) {
+        session.user.roles = token.roles;
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
